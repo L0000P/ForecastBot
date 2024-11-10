b23c1431a300a51f7db46aee3fb5a703e62e6c42
L0000P <alessandro.tomarchio01@gmail.com>
2024-11-10 13:09:20 +0100

    Fix prod entrypoint

--- server/entrypoint-prod.sh
+++ server/entrypoint-prod.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-RUN python /server/src/transformer/Arima/test.py
-RUN python /server/src/transformer/Sarimax/test.py
-RUN python /server/src/transformer/PatchTST/test.py
+python /server/src/transformer/Arima/test.py
+python /server/src/transformer/Sarimax/test.py
+python /server/src/transformer/PatchTST/test.py
 uvicorn src.main:app --host 0.0.0.0 --port 8000
