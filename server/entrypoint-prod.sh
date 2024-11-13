ffb164e36dae595bb651671978dd5fb9af2d54ef
L0000P <alessandro.tomarchio01@gmail.com>
2024-11-13 15:50:15 +0100

    Update prod entrypoint to conditionally run test scripts

--- server/entrypoint-prod.sh
+++ server/entrypoint-prod.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
-python /server/src/transformer/Arima/test.py
-python /server/src/transformer/Sarimax/test.py
-python /server/src/transformer/PatchTST/test.py
+
+if [ ! -d "/server/src/transformer/Arima/results" ]; then
+    python /server/src/transformer/Arima/test.py
+fi
+
+if [ ! -d "/server/src/transformer/Sarimax/results" ]; then
+    python /server/src/transformer/Sarimax/test.py
+fi
+
+if [ ! -d "/server/src/transformer/PatchTST/results" ]; then
+    python /server/src/transformer/PatchTST/test.py
+fi
+
 uvicorn src.main:app --host 0.0.0.0 --port 8000
